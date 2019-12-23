8960d8cccf83043a036696b5bd7202d59a3a620a
Rafael <rafael.engenheiro.qa@gmail.com>
2019-12-23 09:37:06 -0300

    Comite Direto

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -9,43 +9,54 @@ public static void main(String[] args) {
 	int numero1 = 0;
 	int numero2 = 0;
 	int resultado = 0;
-	int op;
+	int op = 0 ;
+	int contador = 0;
 	
+	while (op < 5) {
+    	
+    	op ++;
 	
+    	
 	System.out.println("Digite o primeiro Numero: ");
 		numero1 = entrada.nextInt();	
 		
 		System.out.println("Digite o segundo Numero: ");
 		numero2 = entrada.nextInt();	
 		
-		System.out.println(" Digite a Operacao: 0 1 2 3");
+		System.out.println(" Digite a Operacao: 1 2 3 4");
 	    op = entrada.nextInt();
-		
+	    	    
+	    	
 	switch (op) {
 	
-	case 0 : resultado = numero1 + numero2; 
+	case 1 : resultado = numero1 + numero2; 
 	break;
 	
-	case 1: resultado = numero1 - numero2;
+	case 2: resultado = numero1 - numero2;
 	break;
 	
-	case 2: resultado = numero1 * numero2;
+	case 3: resultado = numero1 * numero2;
 	break;
 	
-	case 3 : resultado = numero1 / numero2;
+	case 4 : resultado = numero1 / numero2;
 	break;
 
 	default:
 	throw new IllegalArgumentException(" Opcao Invalida ");
 	
-	}	
+	}	 
 	
 	System.out.printf(" O Resultado do Calculo e %d ", resultado);
+	System.out.println("");
+	System.out.println("");
+	System.out.println(" Digite a Operacao Novamente ate 5 VEZES");
+	System.out.println("");
 	
-	
-	
+			
+	}
 }
-	
+
 }
+
 	
 
